4447887bddcd5dd582c6183f0062deab5014358c
Dexter <dex429.dc@gmail.com>
2021-08-10 20:59:37 -0400

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-strains = Strain.create([{name: "first strain", category: "Indica", thc: 16.3, cbd: .03, rating: 2, notes: "pretty okay"},
-                        {name: "second strain", category: "Sativa", thc: 19.2, cbd: .03, rating: 3, notes: "pretty good},
-                        {name: "third strain", category: "Hybrid", thc: 20.5, cbd: .03, rating: 4, notes: "good"}                          
+strains = Strain.create([{name: "first strain", category: "Indica", thc: 16.3, cbd: .03, rating: 2, notes: "pretty okay", users.first, growers.first},
+                        {name: "second strain", category: "Sativa", thc: 19.2, cbd: .03, rating: 3, notes: "pretty good, users.first, growers.second},
+                        {name: "third strain", category: "Hybrid", thc: 20.5, cbd: .03, rating: 4, notes: "good", users.first, growers.third}                          
                         ])
 
 
 User.create!(username: "test", email: "test@test.com")
 
-Grower.create!(name: "High Garden")
-Grower.create!(name: "Eden")
-Grower.create!(name: "Cannabliss")
+Grower.create!(name: "High Garden", strains.first)
+Grower.create!(name: "Eden", strains.second)
+Grower.create!(name: "Cannabliss", strains.third)
 
 
